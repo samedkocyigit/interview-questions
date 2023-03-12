f6b5e799b060215157430a069f9258951c3439e2
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-03-12 16:30:06 +0300

    Implementation for my solution

--- InterviewQuestions/Chapters/ArraysStrings/ArraysAndStringSolutions.cs
+++ InterviewQuestions/Chapters/ArraysStrings/ArraysAndStringSolutions.cs
@@ -34,5 +34,33 @@
             }
             return isUniq;
         }
+        public string stringCompression(string input)
+        {
+            string newString = "";
+            int i = 0;
+            while(i< input.Length)
+            {
+                int counter = 1;
+                while (input[i] == input[i+1] && i<input.Length-1)
+                {
+                    counter++;
+                    i++;
+                    if (i == input.Length - 1) {break;} //a limiter for that the index does not exceed the limits of the array
+                }
+                newString += input[i];
+                newString += counter;
+                i++;
+                if (i == input.Length - 1)
+                {
+                    newString += input[i - 1];
+                    newString += counter;
+                    break;
+                }
+            }
+            if (newString.Length < input.Lengt)
+                return newString;
+            else
+                return input;
+        }
     }
 }
