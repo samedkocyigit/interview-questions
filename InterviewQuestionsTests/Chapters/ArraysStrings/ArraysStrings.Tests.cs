9e68f4ad85bc6e53c762468bb430ba68547b42d5
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-04-05 22:33:27 +0300

    created zeroesMatrix function

--- InterviewQuestionsTests/Chapters/ArraysStrings/ArraysStrings.Tests.cs
+++ InterviewQuestionsTests/Chapters/ArraysStrings/ArraysStrings.Tests.cs
@@ -19,37 +19,45 @@ namespace InterviewQuestionsTests.Chapters.ArraysStrings
             bool output = testObject.isStringUniq(input);
             bool outputSecond = testObject.isStringUniqSecondSolution(input);
 
-            
+
             Assert.AreEqual(expectedResult, output);
-            Assert.AreEqual(expectedResult, outputSecond);    
+            Assert.AreEqual(expectedResult, outputSecond);
         }
-        
+
         [DataTestMethod]
-        [DataRow("aaabbcccccdd","a3b2c5d2")]
+        [DataRow("aaabbcccccdd", "a3b2c5d2")]
         [DataRow("aabcde", "aabcde")]
         [DataRow("aaaaabccdeee", "a5b1c2d1e3")]
-        [DataRow("abcdef","abcdef" )]
+        [DataRow("abcdef", "abcdef")]
         public void stringCompression(string input, string expectedResult)
         {
             ArraysAndStringSolutions testObject1 = new ArraysAndStringSolutions();
 
             string solution = testObject1.stringCompression(input);
-            string alternateSolution =testObject1.stringCompressionAlternate(input);
-            
+            string alternateSolution = testObject1.stringCompressionAlternate(input);
+
             Assert.AreEqual(expectedResult, solution);
-            Assert.AreEqual(expectedResult,alternateSolution);
+            Assert.AreEqual(expectedResult, alternateSolution);
         }
         [DataTestMethod]
-        [DataRow("abdulsamed", "demasludba",true)]
-        [DataRow("furkan", "furcan",false)]
-        [DataRow("ali", "lia",true)]
-        public void stringPermutation(string input,string testString,bool expectedResult)
+        [DataRow("abdulsamed", "demasludba", true)]
+        [DataRow("furkan", "furcan", false)]
+        [DataRow("ali", "lia", true)]
+        public void stringPermutation(string input, string testString, bool expectedResult)
         {
             ArraysAndStringSolutions testObjecet2 = new ArraysAndStringSolutions();
 
-            bool solution = testObjecet2.isStringPermutation(input,testString);
+            bool solution = testObjecet2.isStringPermutation(input, testString);
 
-            Assert.AreEqual(expectedResult,solution);
+            Assert.AreEqual(expectedResult, solution);
+        }
+        public void zeroMatrix(int[,] input, int[,] expectedResult)
+        {
+            ArraysAndStringSolutions testObject = new ArraysAndStringSolutions();
+
+            int[,] solution = testObject.zeroesMatrix(input);
+
+            //Assert.AreEqual(expectedResult,solution);
         }
     }
 }
