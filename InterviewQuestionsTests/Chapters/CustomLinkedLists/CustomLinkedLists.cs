bbfa648702f99935f1ad1c331198ac16fd9bf78e
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-04-24 15:24:07 +0300

    Testing right item

--- InterviewQuestionsTests/Chapters/CustomLinkedLists/CustomLinkedLists.cs
+++ InterviewQuestionsTests/Chapters/CustomLinkedLists/CustomLinkedLists.cs
@@ -4,22 +4,26 @@ using InterviewQuestions.Chapters.CustomLinkedLists;
 namespace InterviewQuestionsTests.Chapters.CustomLinkedLists
 {
     [TestClass]
-    
+
     public class CustomLinkedLists
     {
         [TestMethod]
         public void method()
         {
-            CustomLinkedListsMethods list=new CustomLinkedListsMethods();
-            list.add(15);
+            CustomLinkedListsMethods list = new CustomLinkedListsMethods();
+
+            list.add(10);
             list.add(11);
-            list.add(12);   
+            list.add(12);
             list.add(13);
 
-            Assert.AreEqual(list.head.data, 15);
+            Assert.AreEqual(list.head.data, 10);
             Assert.AreEqual(list.head.next.data, 11);
+            Assert.AreEqual(list.head.next.next.data, 12);
+            Assert.AreEqual(list.head.next.next.next.data, 13);
+            Assert.AreEqual(list.head.next.next.next.next, null);
         }
-        
-           
+
+
     }
 }
