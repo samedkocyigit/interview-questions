752a24fc056e84b6a6d1ada498cfdc732811ff22
Samed Kocyigit <102367876+samedkocyigit@users.noreply.github.com>
2023-08-03 13:54:51 +0000

    Implemented deck of cards and deal hand method

---
+++ InterviewQuestions/Chapters/BlackJackGame/BlackJack.cs
@@ -0,0 +1,106 @@
+using static BlackJack.Face;
+
+namespace BlackJack
+{
+    public enum Suit
+    {
+        Clubs,
+        Hearts,
+        Spadec,
+        Diamonds
+    }
+    public enum Face
+    {
+        Ace,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public class Card
+    {
+        public Face face {get;}
+        public Suit suit {get;}
+        public int Value{get;set;}
+
+        public Card(Suit suit,Face face)
+        {
+            Suit=suit;
+            Face=face;
+
+            switch(Face)
+            {
+                case Ten:
+                case Jack:
+                case Queen:
+                case King:
+                    Value=10;
+                    break;
+                case Ace:
+                    Value=11;
+                default:
+                    Value= (int)Face+1;
+                    break;    
+            }
+        }
+    }
+    public class Deck
+    {
+        public List<Card> cards;
+        public Deck()
+        {
+            Initialize();
+        }
+        public List<Card> GetColdDecks()
+        {
+            List<Card> coldDecks = new List<Card>();
+            for(int i = 0; i < 13; i++)
+            {
+                for(int j = 0;j<4;j++)
+                {
+                    coldDecks.Add(new Card((Suit)j,(Face)i));
+                }
+            }
+            return coldDecks;
+        }
+        public List<Card> DealHand()
+        {
+            List<Card> Hand= new List<Card>();
+            Hand.Add(cards[0]);
+            Hand.Add(cards[1]);
+
+            cards.RemoveRange(0, 2);
+            return Hand;
+        }
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            
+            int n= cards.Count;
+            while (n>1)
+            {
+                n--;
+                int k= rng.Next(n+1);
+                Card card=cards[k];
+                cards[k]=cards[n];
+                cards[n]=card;
+            }
+        }
+
+        public void Initialize()
+        {
+            cards = GetColdDecks();
+            Shuffle();
+        }
+    }
+}
