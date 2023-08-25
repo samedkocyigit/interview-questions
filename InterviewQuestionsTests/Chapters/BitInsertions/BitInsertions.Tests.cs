7758d34a7b1aa87985bb842eb81d5af158e2e2e9
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-08-25 15:34:51 +0300

    DataRows added and BitInsertionMethod renamed to bitInsertionTest

--- InterviewQuestionsTests/Chapters/BitInsertions/BitInsertions.Tests.cs
+++ InterviewQuestionsTests/Chapters/BitInsertions/BitInsertions.Tests.cs
@@ -5,15 +5,17 @@ namespace InterviewQuestionsTests.Chapters.BitInsertion
     [TestClass] 
     public class BitInsertionTest
     {
-        [TestMethod]
-        public void BitInsertionMethod()
+        [DataTestMethod]
+        [DataRow(128,19,2,6,204)]
+        [DataRow(65, 12, 1, 4, 89)]
+        [DataRow(2560, 7, 3, 5, 2616)]
+        [DataRow(145, 10, 2, 5, 184)]
+        public void bitInsertionTest(int firstNum, int secNum, int startBit, int endBit, int expectedResult)
         {
             BitInsertions test1 = new BitInsertions();
-            int temp =test1.BitInsertionSolution(128,19,2,6);
-            int temp1 =test1.BitInsertionSolution(65,12,1,4);
+            int temp =test1.BitInsertionSolution(firstNum,secNum,startBit,endBit);
 
-            Assert.AreEqual(temp,204);
-            Assert.AreEqual(temp1,89);
+            Assert.AreEqual(temp, expectedResult);
         }
     }
 
