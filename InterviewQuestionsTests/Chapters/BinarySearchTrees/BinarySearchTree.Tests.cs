abc992266ea9f97d3fa2bdd3f08ab57fbf66ac67
samedkocyigit <abdulsamed.kcygt@gmail.com>
2023-09-07 16:23:44 +0300

    There is two different binary search tree folder were created. Deleted old one

--- InterviewQuestionsTests/Chapters/BinarySearchTrees/BinarySearchTree.Tests.cs
+++ InterviewQuestionsTests/Chapters/BinarySearchTrees/BinarySearchTree.Tests.cs
@@ -1,11 +1,15 @@
-﻿using InterviewQuestions.Chapters.BinarySearchTree;
+﻿using InterviewQuestions.Chapters.BinarySearchTrees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace InterviewQuestionsTests.Chapters.BinarySearchTree
+namespace InterviewQuestionsTests.Chapters.BinarySearchTrees
 {
     [TestClass]
     public class BinarySearchTreeTests
     {
-
         private static int findMinDepth(int length)
         {
             int depth = 0;
@@ -19,32 +23,29 @@ namespace InterviewQuestionsTests.Chapters.BinarySearchTree
             }
             return depth;
         }
-
         [TestMethod]
         public void BinarySearchTreeTest()
         {
-
-            BinarySearchTreeSolutions bts = new BinarySearchTreeSolutions();
+            BinarySearchTree bts = new BinarySearchTree();
             int[] arr1 = new int[] { 14, 17, 20, 28, 32, 42, 52, };
             int n = arr1.Length;
-            BinarySearchTreeSolutions.Node root = bts.sortedArrayToBST(arr1, 0, n - 1);
-            int depth = bts.GetTreeDepth(root);
-            int minDepth = findMinDepth(n);
+            int depth = findMinDepth(n);
+            BinarySearchTree.Node root = bts.sortedArrayToBST(arr1, 0, n - 1);
+            int depthTest = bts.GetTreeDepth(root);
 
-            Assert.AreEqual(depth, minDepth);
+            Assert.AreEqual(depthTest,3 );
         }
         [TestMethod]
         public void BinarySearchTreeTest2()
         {
-
-            BinarySearchTreeSolutions bts = new BinarySearchTreeSolutions();
+            BinarySearchTree bts = new BinarySearchTree();
             int[] arr1 = new int[] { 1, 3, 5, 7, 10, 16, 19, 25 };
             int n = arr1.Length;
-            BinarySearchTreeSolutions.Node root = bts.sortedArrayToBST(arr1, 0, n - 1);
-            int depth = bts.GetTreeDepth(root);
-            int minDepth = findMinDepth(n);
+            int depth =findMinDepth(n);
+            BinarySearchTree.Node root = bts.sortedArrayToBST(arr1, 0, n - 1);
+            int depthTest= bts.GetTreeDepth(root);
 
-            Assert.AreEqual(depth, minDepth);
+            Assert.AreEqual(depthTest,4 );
         }
     }
 }
